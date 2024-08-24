0558ef41cfeedd3b6ff5d35a2ee56d5ed3d984fa
dheerajatulkar <141481745+dheerajatulkar@users.noreply.github.com>
2024-08-24 16:35:01 +0000

    conversion of datatypes

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,24 @@
+let score = 33
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+
+// "33"- 33
+// "33abc" - NaN not an number
+// true - 1; false - 0 
+
+let isLoggedIn ="dheeraj"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1 - true ; 0 - false
+// "" - false
+// "Dheeraj" - true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
