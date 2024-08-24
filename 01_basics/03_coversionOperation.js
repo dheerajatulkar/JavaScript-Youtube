58f9e39f9047482f648a8453ea0ce11c58373cea
dheerajatulkar <141481745+dheerajatulkar@users.noreply.github.com>
2024-08-24 16:50:53 +0000

    coversion opertation

--- 01_basics/03_coversionOperation.js
+++ 01_basics/03_coversionOperation.js
@@ -20,5 +20,65 @@ let booleanisLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// *********************OPERATIONS*************************
+
+
+let value=3
+let negValue= -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);power
+// console.log(2/2);
+// console.log(2%2);reminder
+
+let str1= "hello"
+let str2= " dheeraj"
+let str3= str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); // dont write like all this in one use brackets
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
