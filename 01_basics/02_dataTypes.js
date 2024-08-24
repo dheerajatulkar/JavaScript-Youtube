358dc889e333266a3819bb95116264ab9d344f94
dheerajatulkar <141481745+dheerajatulkar@users.noreply.github.com>
2024-08-24 16:14:53 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs,not browser
+
+console.log(3+3)//code reddability should be high
+console.log("Dheeraj")
+
+
+let name = "Dheeraj"
+let age = 23
+let isLoggedIn = false
+let state;
+// number=> 2 to power 53
+// bigint - very big number
+// string - ""
+// boolean = true/false
+// null -stand alone value
+// undefined - no value given
+// symbol - unique
+
+
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
