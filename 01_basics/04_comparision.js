e7a8731297e855e4e8339a7c16cdf5ad6181185a
dheerajatulkar <141481745+dheerajatulkar@users.noreply.github.com>
2024-08-24 17:01:27 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+
+// console.log("02" > 1);
+
+
+// console.log(null > 0); comparision converts null to number,treating it as 0
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// === strictly check(checks value and datatypes)
+
+console.log("2" === 2);
+
